47ac10d9f0ac87f9f31f44b516ff012575a5d954
JEONG <jungds95@naver.com>
2023-02-03 14:04:43 +0900

    feat : [006-CUSTOMER_balance] 고객에 대한 잔액 조회 구현

--- cms/zero-base-cms/zero-base-cms/user-api/src/main/java/com/zerobase/cms/user/controller/CustomerController.java
+++ cms/zero-base-cms/zero-base-cms/user-api/src/main/java/com/zerobase/cms/user/controller/CustomerController.java
@@ -1,18 +1,16 @@
 package com.zerobase.cms.user.controller;
 
+import com.zerobase.cms.user.domain.customer.ChangeBalanceForm;
 import com.zerobase.cms.user.domain.customer.CustomerDto;
 import com.zerobase.cms.user.domain.model.Customer;
 import com.zerobase.cms.user.exception.CustomException;
-import com.zerobase.cms.user.exception.ErrorCode;
 import com.zerobase.cms.user.service.CustomerService;
+import com.zerobase.cms.user.service.CustomerBalanceService;
 import com.zerobase.domain.config.JwtAuthenticationProvider;
 import com.zerobase.domain.domain.common.UserVo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static com.zerobase.cms.user.exception.ErrorCode.NOT_FOUND_USER;
 
@@ -23,6 +21,7 @@ public class CustomerController {
 
     private final JwtAuthenticationProvider provider;
     private final CustomerService customerService;
+    private final CustomerBalanceService customerBalanceService;
 
     @GetMapping("/getInfo")
     public ResponseEntity<CustomerDto> getInfo(@RequestHeader(name = "X-AUTH-TOKEN") String token){
@@ -33,4 +32,11 @@ public class CustomerController {
         return ResponseEntity.ok(CustomerDto.from(c));
     }
 
+    @PostMapping("/balance")
+    public ResponseEntity<Integer> changeBalance(@RequestHeader(name = "X-AUTH-TOKEN") String token,
+                                                 @RequestBody ChangeBalanceForm form){
+        UserVo vo = provider.getUserVo(token);
+
+        return ResponseEntity.ok(customerBalanceService.changeBalance(vo.getId(), form).getCurrentMoney());
+    }
 }
