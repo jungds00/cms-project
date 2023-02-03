43697527e3eb7058ffd435dd95b905295af3a443
JEONG <jungds95@naver.com>
2023-02-03 13:56:29 +0900

    feat : [005-SELLER_SIGN_UP_IN] 셀러에 대한 사인업, 사인인 구현
    seller/* 에 대한 token filter 적용

--- cms/zero-base-cms/zero-base-cms/user-api/src/main/java/com/zerobase/cms/user/application/SignInApplication.java
+++ cms/zero-base-cms/zero-base-cms/user-api/src/main/java/com/zerobase/cms/user/application/SignInApplication.java
@@ -2,8 +2,10 @@ package com.zerobase.cms.user.application;
 
 import com.zerobase.cms.user.domain.SignInForm;
 import com.zerobase.cms.user.domain.model.Customer;
+import com.zerobase.cms.user.domain.model.Seller;
 import com.zerobase.cms.user.exception.CustomException;
-import com.zerobase.cms.user.service.CustomerService;
+import com.zerobase.cms.user.service.customer.CustomerService;
+import com.zerobase.cms.user.service.seller.SellerService;
 import com.zerobase.domain.config.JwtAuthenticationProvider;
 import com.zerobase.domain.domain.common.UserType;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +18,7 @@ import static com.zerobase.cms.user.exception.ErrorCode.LOGIN_CHECK_FAIL;
 public class SignInApplication{
 
     private final CustomerService customerService;
+    private final SellerService sellerService;
     private final JwtAuthenticationProvider provider;
 
     public String customerLoginToken(SignInForm form){
@@ -25,5 +28,12 @@ public class SignInApplication{
         //2. 토큰을 발행하고
         //3. 토큰을 response한다.
         return provider.createToken(c.getEmail(),c.getId(), UserType.CUSTOMER);
+    }
+    public String sellerLoginToken(SignInForm form){
+        //1. 로그인 가능 여부
+        Seller s = sellerService.findValidSeller(form.getEmail(),form.getPassword())
+                .orElseThrow(()->new CustomException(LOGIN_CHECK_FAIL));
+
+        return provider.createToken(s.getEmail(),s.getId(), UserType.SELLER);
     }
 }
--- cms/zero-base-cms/zero-base-cms/user-api/src/main/java/com/zerobase/cms/user/controller/SignUpController.java
+++ cms/zero-base-cms/zero-base-cms/user-api/src/main/java/com/zerobase/cms/user/controller/SignUpController.java
@@ -4,29 +4,33 @@ import com.zerobase.cms.user.application.SignUpApplication;
 import com.zerobase.cms.user.domain.SignUpForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping({"/signup"})
+@RequiredArgsConstructor
 public class SignUpController {
     private final SignUpApplication signUpApplication;
 
-    @PostMapping
+    @PostMapping("/customer")
     public ResponseEntity<String> customerSignUp(@RequestBody SignUpForm form) {
-        return ResponseEntity.ok(this.signUpApplication.customerSignUp(form));
+        return ResponseEntity.ok(signUpApplication.customerSignUp(form));
     }
 
-    @PutMapping({"/verify/customer"})
+    @GetMapping({"/customer/verify"})
     public ResponseEntity<String> verifyCustomer(String email, String code) {
-        this.signUpApplication.customerVerify(email, code);
+        signUpApplication.customerVerify(email, code);
         return ResponseEntity.ok("인증이 완료되었습니다");
     }
 
-    public SignUpController(final SignUpApplication signUpApplication) {
-        this.signUpApplication = signUpApplication;
+    @PostMapping("/seller")
+    public ResponseEntity<String> sellerSignUp(@RequestBody SignUpForm form) {
+        return ResponseEntity.ok(signUpApplication.sellerSignUp(form));
+    }
+
+    @GetMapping({"/seller/verify"})
+    public ResponseEntity<String> verifySeller(String email, String code) {
+        signUpApplication.sellerVerify(email, code);
+        return ResponseEntity.ok("인증이 완료되었습니다");
     }
 }
