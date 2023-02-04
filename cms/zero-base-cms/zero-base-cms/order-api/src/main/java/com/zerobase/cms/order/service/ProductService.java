62df8e6895e786fe8ff5ee4f70053b98fa5b4641
JEONG <jungds95@naver.com>
2023-02-04 19:50:17 +0900

    feat : [010-Product-MODIFY] Product & ProductionItem Modify
    Product & ProductionItem Modify

--- cms/zero-base-cms/zero-base-cms/order-api/src/main/java/com/zerobase/cms/order/service/ProductService.java
+++ cms/zero-base-cms/zero-base-cms/order-api/src/main/java/com/zerobase/cms/order/service/ProductService.java
@@ -1,8 +1,13 @@
 package com.zerobase.cms.order.service;
 
 import com.zerobase.cms.order.domain.model.Product;
+import com.zerobase.cms.order.domain.model.ProductItem;
 import com.zerobase.cms.order.domain.product.AddProductForm;
+import com.zerobase.cms.order.domain.product.UpdateProductForm;
+import com.zerobase.cms.order.domain.product.UpdateProductItemForm;
 import com.zerobase.cms.order.domain.repository.ProductRepository;
+import com.zerobase.cms.order.exception.CustomException;
+import com.zerobase.cms.order.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,4 +22,21 @@ public class ProductService {
         return productRepository.save(Product.of(sellerId,form));
     }
 
+    @Transactional
+    public Product updateProduct(Long sellerId, UpdateProductForm form){
+        Product product = productRepository.findBySellerIdAndId(sellerId,form.getId())
+                .orElseThrow(()->new CustomException(ErrorCode.NOT_FOUND_PRODUCT));
+        product.setName(form.getName());
+        product.setDescription(form.getDescription());
+
+        for(UpdateProductItemForm itemForm : form.getItems()){
+            ProductItem item = product.getProductItems().stream()
+                    .filter(pi->pi.getId().equals(itemForm.getId()))
+                    .findFirst().orElseThrow(()-> new CustomException(ErrorCode.NOT_FOUND_ITEM));
+            item.setName(itemForm.getName());
+            item.setPrice(itemForm.getPrice());
+            item.setCount(itemForm.getCount());
+        }
+        return product;
+    }
 }
