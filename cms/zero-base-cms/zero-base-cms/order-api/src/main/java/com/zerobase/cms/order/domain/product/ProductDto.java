8b330d8479ef3960258ef93db9955c741df39753
JEONG <jungds95@naver.com>
2023-02-05 13:41:12 +0900

    feat : [012-Product_SEARCH] Product에 대한 검색 기능 구현

--- cms/zero-base-cms/zero-base-cms/order-api/src/main/java/com/zerobase/cms/order/domain/product/ProductDto.java
+++ cms/zero-base-cms/zero-base-cms/order-api/src/main/java/com/zerobase/cms/order/domain/product/ProductDto.java
@@ -29,4 +29,15 @@ public class ProductDto {
                 .items(items)
                 .build();
     }
+    public static ProductDto withoutItemsfrom(Product product){
+        /*List<ProductItemDto> items = product.getProductItems()
+                .stream().map(ProductItemDto::from).collect(Collectors.toList());*/
+
+        return ProductDto.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+        //        .items(items)
+                .build();
+    }
 }
